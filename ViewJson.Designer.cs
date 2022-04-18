be057ad91e2335ada54dc4effafe4fc0f5578df7
udayKumar2u <udaykumar2u@gmail.com>
2022-04-18 18:50:57 +0530

    Add files via upload

---
+++ ViewJson.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace JsonEditor
+{
+    partial class ViewJson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgTasks = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgTasks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgTasks
+            // 
+            this.dgTasks.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dgTasks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgTasks.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgTasks.Location = new System.Drawing.Point(99, 311);
+            this.dgTasks.Margin = new System.Windows.Forms.Padding(4);
+            this.dgTasks.Name = "dgTasks";
+            this.dgTasks.RowHeadersWidth = 51;
+            this.dgTasks.Size = new System.Drawing.Size(1203, 359);
+            this.dgTasks.TabIndex = 0;
+            this.dgTasks.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgTasks_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.button1.Location = new System.Drawing.Point(417, 100);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open Json File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkBlue;
+            this.button2.Location = new System.Drawing.Point(593, 100);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save Json File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(417, 181);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(334, 33);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Tag to Edit";
+            // 
+            // ViewJson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1637, 724);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgTasks);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "ViewJson";
+            this.Text = "Json Files Update";
+            ((System.ComponentModel.ISupportInitialize)(this.dgTasks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgTasks;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ViewJson.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace JsonEditor
+{
+    public partial class ViewJson : Form
+    {
+
+        DataSet DsData = new DataSet();
+        private BindingSource bindingSource1 = new BindingSource();
+        public ViewJson()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "JSON files|*.json";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (theDialog.FileName.Trim() != string.Empty)
+                    {
+                        using (StreamReader r = new StreamReader(theDialog.FileName))
+                        {
+                            string json = r.ReadToEnd();
+                            DsData = ReadDataFromJson(json, XmlReadMode.InferTypedSchema);
+                           // var dsData = dataSet;
+
+                            string dsxml = ds2json(DsData);
+                            dgTasks.DataSource = DsData.Tables[1].DefaultView;
+                            dgTasks.DataSource = bindingSource1;
+                            dgTasks.AutoSizeRowsMode =
+               DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+                            dgTasks.BorderStyle = BorderStyle.Fixed3D;
+                            dgTasks.EditMode = DataGridViewEditMode.EditOnEnter;
+                            for (int i = 0; i < DsData.Tables.Count-1; i++)
+                            {
+                                
+
+                                comboBox1.Items.Add(DsData.Tables[i].TableName);
+                            }
+                            
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         //   Tasks tasks = new Tasks();
+          //  tasks.tasks = (List<Task>)dgTasks.DataSource;
+            
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @"C:\";
+            saveFileDialog1.Filter = "JSON Image|*.json";
+            saveFileDialog1.Title = "Save a JSON File";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, JsonConvert.SerializeObject(DsData, Newtonsoft.Json.Formatting.Indented));
+            }
+
+        }
+
+        private DataSet ReadDataFromJson(string jsonString, XmlReadMode mode = XmlReadMode.Auto)
+        {
+            //// Note:Json convertor needs a json with one node as root
+            jsonString = $"{{ \"rootNode\": {{{jsonString.Trim().TrimStart('{').TrimEnd('}')}}} }}";
+            //// Now it is secure that we have always a Json with one node as root 
+            var xd = JsonConvert.DeserializeXmlNode(jsonString);
+
+            //// DataSet is able to read from XML and return a proper DataSet
+            var result = new DataSet();
+            result.ReadXml(new XmlNodeReader(xd), mode);
+            return result;
+
+
+        }
+
+        public string ds2json(DataSet ds)
+        {
+            return JsonConvert.SerializeObject(ds, Newtonsoft.Json.Formatting.Indented);
+        }
+
+        private void dgTasks_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dgTasks.DataSource = DsData.Tables[comboBox1.SelectedItem.ToString()];
+        }
+    }
+}
